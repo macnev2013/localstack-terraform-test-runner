5aabf6e79e8ad005026f2a206123a60d94a6aee9
macnev2013 <macnev2013@gmail.com>
2022-08-17 22:54:41 +0530

    updated concurrency

--- run.sh
+++ run.sh
@@ -8,7 +8,8 @@ export AWS_DEFAULT_REGION=us-east-1
 export AWS_ACCESS_KEY_ID=test
 export AWS_SECRET_ACCESS_KEY=test
 
-PARALLELISM_MAPPING=( "route53resolver:4")
+PARALLELISM_MAPPING=( "route53resolver:4"
+    "ec2:4")
 # example
 # PARALLELISM_MAPPING=( "route53resolver:4"
 #         "s3:2"
